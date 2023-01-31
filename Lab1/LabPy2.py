4accda7fbf4fae0e6d6a28812accc499f6ece725
Jose Vitor Pieroni <vitorpieroni1@gmail.com>
2023-01-31 11:48:59 +0000

    LabPy2 updates

--- Lab1/LabPy2.py
+++ Lab1/LabPy2.py
@@ -1,1 +1,15 @@
-#check if string is equal as to the reverse
+# check if string is equal as to the reverse
+# check the first character to the last character of the string and second to second last one 
+# and so on …. If any character mismatches, the string wouldn’t be a palindrome.
+
+def isPalindrome(str):
+ 
+    # Run loop from 0 to len/2
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+
+userInput = input("Enter word:")
+print(isPalindrome(userInput))
+
